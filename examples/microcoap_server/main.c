c16b94ba30bce2c86f6f63c6589e9f6618b061fa
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2016-03-11 16:06:39 +0100

    microcoap-server: Auto init net

--- examples/microcoap_server/main.c
+++ examples/microcoap_server/main.c
@@ -19,6 +19,8 @@
 
 #include <stdio.h>
 #include "msg.h"
+#include "net/gnrc/rpl.h"
+#include "net/gnrc/netapi.h"
 
 #define MAIN_QUEUE_SIZE     (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
@@ -28,12 +30,41 @@ void microcoap_server_loop(void);
 /* import "ifconfig" shell command, used for printing addresses */
 extern int _netif_config(int argc, char **argv);
 
+void init_net(void)
+{
+    uint16_t val;
+    int res;
+    kernel_pid_t dev = 6;
+
+    val = 0;
+    res = gnrc_netapi_set(dev, NETOPT_CHANNEL, 0, &val, sizeof(uint16_t));
+    if (res < 0) {
+        printf("Unable to set channel %"PRIu16", res=%d\n", val, res);
+    }
+
+    val = 0;
+    res = gnrc_netapi_set(dev, NETOPT_CHANNEL_PAGE, 0, &val, sizeof(uint16_t));
+    if (res < 0) {
+        printf("Unable to set page %"PRIu16", res=%d\n", val, res);
+    }
+
+    val = 0x777;
+    res = gnrc_netapi_set(dev, NETOPT_NID, 0, &val, sizeof(uint16_t));
+    if (res < 0) {
+        printf("Unable to set PAN ID 0x%"PRIx16", res=%d\n", val, res);
+    }
+
+    gnrc_rpl_init(dev);
+}
+
 int main(void)
 {
     puts("RIOT microcoap example application");
 
     /* microcoap_server uses conn which uses gnrc which needs a msg queue */
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
+
+    init_net();
 
     /* print network addresses */
     puts("Configured network interfaces:");
