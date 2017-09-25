c2699192514868ec2c5307e1c6a938c112ed1604
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2017-09-25 08:42:19 +0200

    squash kw41zrf clocking

--- drivers/kw41zrf/kw41zrf.c
+++ drivers/kw41zrf/kw41zrf.c
@@ -142,8 +142,8 @@ void kw41zrf_reset_phy(kw41zrf_t *dev)
     /* Configure DSM exit oscillator stabilization delay */
     uint32_t tmp = (RSIM->RF_OSC_CTRL & RSIM_RF_OSC_CTRL_BB_XTAL_READY_COUNT_SEL_MASK) >>
         RSIM_RF_OSC_CTRL_BB_XTAL_READY_COUNT_SEL_SHIFT;
-    /* Stabilization time is 1024 * 2^x core clocks, 0 <= x <= 3 */
-    RSIM->DSM_OSC_OFFSET = (1024ul << tmp) / (CLOCK_CORECLOCK / 32768) + 1; /* round up */
+    /* Stabilization time is 1024 * 2^x radio crystal clocks, 0 <= x <= 3 */
+    RSIM->DSM_OSC_OFFSET = (1024ul << tmp) / (CLOCK_RADIOXTAL / 32768) + 1; /* round up */
 
     /* Bring the device out of low power mode */
     kw41zrf_set_power_mode(dev, KW41ZRF_POWER_IDLE);
